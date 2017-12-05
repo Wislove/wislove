4879d3650e8c3912ebb218876a836329c8606e80
廖双龙 <975385373@qq.com>
2017-12-05 18:23:18 +0800

    增加gateway项目，在common增加公用配置文件，整合spring

--- wislove-config/src/main/java/com/wislove/config/Config.java
+++ wislove-config/src/main/java/com/wislove/config/Config.java
@@ -1,9 +1,17 @@
 package com.wislove.config;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
 /**
  * Created by 廖双龙 on 2017/7/27.
  */
 public class Config {
 
-
+    public static  void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        context.start();
+        System.in.read();
+    }
 }
