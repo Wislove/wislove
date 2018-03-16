e975f143fac6ed9dbd829dfe74dd6b86a6837cef
廖双龙 <975385373@qq.com>
2018-03-16 18:13:27 +0800

    增加用户中心spring-boot的配置

---
+++ wislove-spring-boot/src/main/java/com/wislove/uc/main/SpringBootUcMain.java
@@ -0,0 +1,15 @@
+package com.wislove.uc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 廖双龙 on 2018/3/16.
+ */
+@SpringBootApplication
+public class SpringBootUcMain {
+
+    public static  void main(String[] args){
+        SpringApplication.run(SpringBootUcMain.class);
+    }
+}
