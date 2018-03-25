eb78ea8f0249a331e58be5a5e40ef009eac3d5cd
liaosl <975385373@qq.com>
2018-03-25 22:18:00 +0800

    logback的整合配置

--- wislove-uc/src/main/java/com/wislove/uc/controller/UserController.java
+++ wislove-uc/src/main/java/com/wislove/uc/controller/UserController.java
@@ -3,6 +3,9 @@ package com.wislove.uc.controller;
 import com.wislove.uc.entity.UserBaseInfo;
 import com.wislove.uc.service.LoginService;
 import com.wislove.uc.service.RegisterService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 @EnableAutoConfiguration
 @RequestMapping("/uc")
 public class UserController {
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Resource
     private LoginService loginService;
@@ -28,9 +32,9 @@ public class UserController {
 
    @RequestMapping(value = "/login")
    public UserBaseInfo login(String account, String password, HttpServletRequest request, HttpServletResponse response){
-        UserBaseInfo userBaseInfo = loginService.login(account, password);
-
-        return userBaseInfo;
+       logger.info("调用登陆接口");
+       UserBaseInfo userBaseInfo = loginService.login(account, password);
+       return userBaseInfo;
    }
 
     @RequestMapping(value = "/register")
