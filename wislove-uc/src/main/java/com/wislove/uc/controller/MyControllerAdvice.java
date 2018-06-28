ac442c7ed1b013b178178f18265828e0e1afc0da
廖双龙 <975385373@qq.com>
2018-06-28 17:20:42 +0800

    修改统一的异常处理方式

--- wislove-uc/src/main/java/com/wislove/uc/controller/MyControllerAdvice.java
+++ wislove-uc/src/main/java/com/wislove/uc/controller/MyControllerAdvice.java
@@ -8,23 +8,37 @@ import com.wislove.api.ApiResult;
 import com.wislove.exception.ParamsException;
 
 /**
-* 统一的异常处理类
-* @author: 廖双龙
-* @date: 2018年6月28日下午2:48:18  
-*/
+ * 统一的异常处理类
+ * 
+ * @author: 廖双龙
+ * @date: 2018年6月28日下午2:48:18
+ */
 @RestControllerAdvice
 public class MyControllerAdvice {
 	
-	 @ExceptionHandler({ ParamsException.class })
-	 public ApiResult handleParamsException(ParamsException exception){
-		 System.out.println(exception.getClass().getName());
-		 return ApiResult.fail(ApiCode.PARAM_EXCEPTION.getCode(), exception.getMessage());	 
-	 }
-	 
-	 @ExceptionHandler({ Exception.class })
-	 public ApiResult handleException(Exception exception){
-		 System.out.println(exception.getClass().getName());
-		 System.out.println(ApiCode.PARAM_EXCEPTION.getCode());
-		 return ApiResult.fail(ApiCode.PARAM_EXCEPTION.getCode(), exception.getMessage());	 
-	 }
+	/**
+	 * 处理参数异常
+	 * @param exception
+	 * @return ApiResult
+	 * @author 廖双龙
+	 * @date 2018年6月28日 下午5:18:10
+	 * @version v1.0.0
+	 */
+	@ExceptionHandler({ ParamsException.class })
+	public ApiResult handleParamsException(ParamsException exception) {
+		return ApiResult.fail(ApiCode.PARAM_EXCEPTION.getCode(), exception.getMessage());
+	}
+	
+	/**
+	 * 处理其他异常
+	 * @param exception
+	 * @return ApiResult
+	 * @author 廖双龙
+	 * @date 2018年6月28日 下午5:18:23
+	 * @version v1.0.0
+	 */
+	@ExceptionHandler({ Exception.class })
+	public ApiResult handleException(Exception exception) {
+		return ApiResult.fail(ApiCode.PARAM_EXCEPTION.getCode(), exception.getMessage());
+	}
 }
